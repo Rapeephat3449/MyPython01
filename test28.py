122b0f8ca1435966a8a9b09e245c1f8401df1d98
supervanilla254 <mmmnd01@gmail.com>
2025-09-05 12:06:27 +0700

    V.7.1

---
+++ test28.py
@@ -0,0 +1,10 @@
+# Function คือ การทำงานหนึ่งๆ จะไม่ทำงานหากไม่เรียกใช้ (call function)
+
+# Function มีที่มาที่ไปยังไงบ้าง
+# - Standard Function/Build-in Function มากับภาษาโปรแกรม
+# - User Define Function ฟังก์ชันที่ Dev สร้างขึ้นมาเอง
+# - Third-Party Function ฟังก์ชันที่ คนอื่นๆ เขาสร้างไว้
+
+# Function มี 4 ประเภท
+# - no parameter no return
+# - have parameter no return
---
+++ test29.py
@@ -0,0 +1,26 @@
+# 1.function: no parameter no return
+# parameter คือ ตัวแปรประเภทหนึ่ง ที่ใช้ได้เฉพาะฟังก์ชันนั้นๆ เท่านั้น เขียนอยู่ในวงเล็บหลังชื่อฟังก์ชัน
+# return คือ คำสั่งที่บงบอกถึงการจบการทำงานของฟังฏ์ชันนั้นๆ และส่งค่าที่อยู่หลัง return กลับไปที่จุดเรียกฟังก์ชัน
+
+'''
+def func_name():
+    คำสั่ง
+    คำสั่ง
+    คำสั่ง
+    ....
+'''
+
+print('wow')
+
+def showHI():
+    print('HI')
+
+print('woo')
+
+def showHEY():
+    print('HEY')
+
+print('wii')
+
+showHI()
+showHEY()
